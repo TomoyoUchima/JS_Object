6f07a01bfe375102b9d72c2435df86f168c692e9
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-28 15:23:28 +0900

    spliceで配列を変更

--- main.js
+++ main.js
@@ -3,13 +3,10 @@
 
 const a = [1, 5, 10];
 
-// a.unshift(100); 最初に追加
-// a.push(200, 300); 末尾に追加
-
-a.shift(); //最初を削除
-a.pop(); //末尾を削除
+a.splice(2, 0, 6, 7); //a.splice(始める箇所, 削除する数, 追加する要素)
+const removed = a.splice(1, 2); //5と6を削除
 
 console.log(a);
-
+console.log(removed); //削除した 5,6を取得
 
 }
