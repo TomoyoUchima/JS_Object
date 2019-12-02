15a1a2cb5ae2011d4d419257ee7cfd23f9f526e1
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-02 11:32:37 +0900

    forEach(繰り返し)

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 'use strict'
 {
 
-const a = [1, 5, 10];
+const a = [1, 5, 10, 100];
 
-//要素の削除と追加
-a.splice(2, 0, 6, 7); //a.splice(始める箇所, 削除する数, 追加する要素)
-const removed = a.splice(1, 2); //5と6を削除
+// for (let i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
 
-console.log(a);
-// console.log(removed); //削除した 5,6を取得
+// a.forEach(item => {
+// console.log(item);
+// });
+
+a.forEach((item, index) => {
+  console.log(`${index}:${item}`);
+});
+
+
 
 }
