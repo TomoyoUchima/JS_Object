1171902d44cc58605b26dd54811c8fd7a82ab4ed
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-02 11:17:22 +0900

    splice(配列要素の追加や削除)

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
 
 const a = [1, 5, 10];
 
+//要素の削除と追加
 a.splice(2, 0, 6, 7); //a.splice(始める箇所, 削除する数, 追加する要素)
 const removed = a.splice(1, 2); //5と6を削除
 
 console.log(a);
-console.log(removed); //削除した 5,6を取得
+// console.log(removed); //削除した 5,6を取得
 
 }
