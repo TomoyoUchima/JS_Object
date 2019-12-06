c5500354306d2aac7efbb396e6a82f5eaecc10b2
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-06 13:43:43 +0900

    Mathオブジェクト

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 'use strict'
 {
 
-  const str = 'hello';
+// console.log(Math.PI);
 
-  // console.log(str.length); //5
-  // console.log(str.substring(2, 4));
-  console.log(str[1]); //4 文字列は、個々の文字に配列のようにアクセスできる。
+// console.log(Math.random()); // 0 .... 0.9999 (0以上1未満のランダムな数値を生成)
 
+// Math.random() ->  0 .... 0.9999
+// Math.random() * 6 ->  0 .... 5.9999
+// Math.random() * 6 + 1 ->  0 .... 6.9999
+// Math.floor(Math.random() * 6 + 1) ->  1 .... 6
+
+console.log(Math.floor(Math.random() * 6 + 1));
 };
