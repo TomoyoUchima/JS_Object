77f558c3461b64a24967dc407cc79008019ac958
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-06 13:25:25 +0900

    分割代入

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 'use strict'
 {
 
-  // const a = [10, 20];
-  // const b = [1, 2, ...a]; // [1, 2, 10, 20]
-  // // ※配列やオブジェクトはコピーされると参照が渡されるだけという特徴がありましたが、スプレッド演算子をこのように渡したときはそうではなくて、値のコピーがそのままこちらで展開される、という意味になる。
-
-  // // このため、aの中身を変更してもbの結果は変わらない。
-  // a[0] = 100;
-  // console.log(b); // [1, 2, 10, 20] ([1, 2, 100, 20]にはならない)
-
-
-  //関数の引数にも使える
-  // const a = [10, 20];
-  // const sum = (a, b) => a + b;
-  // console.log(sum(...a)); //30
+  // const numbers = [1, 2];
+  // // const a = numbers[0];
+  // // const b = numbers[1];
+  // const [a, b] = numbers;
+  // console.log(a)
+  // console.log(b)
 
 
-  //オブジェクトの例
-  const o1 = {a: 1};
-  const o2 = {...o1, b: 2};
-  console.log(o2); //{a: 1, b: 2}
+  //分割代入を使うパターン
+  // const numbers = [1, 2];
+  // const [a, b] = numbers;
+  // console.log(a)
+  // console.log(b)
 
+
+  // const numbers = [1, 2, 3, 8];
+  // const [a, b, ...rest] = numbers;
+  // console.log(a)
+  // console.log(b)
+  // console.log(rest);
+
+const player = {
+  name: 'taguchi',
+  score: 55,
+  hp: 33,
+  mp: 22,
+}
+const {name, score, ...points} = player;
+console.log(name);
+console.log(score);
+console.log(points);
 };
