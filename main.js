b68a2a889659b20ef3f029a5cc29e57cc260c8a9
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-06 13:58:34 +0900

    Dateオブジェクト

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 'use strict'
 {
 
-// console.log(Math.PI);
+const d = new Date();
+// console.log(d); //Fri Dec 06 2019 13:49:21 GMT+0900 (日本標準時)
 
-// console.log(Math.random()); // 0 .... 0.9999 (0以上1未満のランダムな数値を生成)
+console.log(d.getFullYear());
+console.log(d.getMonth()); // 1月 - 12月 → 0 -11
+console.log(d.getDate());
+console.log(d.getDay()); // 日曜 - 土曜 → 0 - 6
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getMilliseconds());
 
-// Math.random() ->  0 .... 0.9999
-// Math.random() * 6 ->  0 .... 5.9999
-// Math.random() * 6 + 1 ->  0 .... 6.9999
-// Math.floor(Math.random() * 6 + 1) ->  1 .... 6
-
-console.log(Math.floor(Math.random() * 6 + 1));
+console.log(d.getTime()); //UTC 1970/01/01 00:00:00
+// → 1970/01/01 00:00:00 の基準日から現在日時までの経過ミリ秒が表示される
 };
