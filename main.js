95c1d9d94f1534893dd92e1b624dc7a9d90548a7
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-09 10:33:43 +0900

    setTimeout

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 'use strict'
 {
-  let i = 0;
-  
-  const showTime = () => {
-    console.log(new Date());
-    i++;
-    if (i > 2) {
-      clearInterval(timerId);
-    }
-  }
+ 
+let i = 0
+const showTime = () => {
+console.log(new Date());
+let timerId = setTimeout(showTime, 1000);
+i++;
+if (i > 2) {
+  clearTimeout(timerId);
+}
 
-  // setInterval(関数, ミリ秒);
-  // setInterval() は、関数を渡してあげて、ミリ秒を渡すと、「この関数の処理をこのミリ秒間隔で繰り返し実行してくれる」という命令。
-  // setInterval(showTime, 1000); //止める処理が必要
+};
 
-  let timerId = setInterval(showTime, 1000);
+showTime();
 
-  // ※setInterval() は、きっちりこの間隔(今回の場合1000ミリ秒)で実行しようとするので、もしこちらの処理に時間がかかってしまう場合、前の処理が終わっていなくても次の処理を実行しようとする。その結果、システムに負荷をかけてしまうこともあるので注意。
 
 };
